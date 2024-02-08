c765669a7763a907abd0cb624e34360aa278e05b
Martin <34022590+mmeest@users.noreply.github.com>
2024-02-08 20:10:22 +0200

    Update Fetch_API.js

--- Fetch_API.js
+++ Fetch_API.js
@@ -8,3 +8,16 @@ fetch("https://swapi.dev/api/people/1")
   console.log("ERROR!", e);
 });
 
+//////////////////////////////////////////////
+
+const loadStarWarsPeople = async () => {
+  try{
+  const res = await fetch("https://swapi.dev/api/people/1/");
+  const data = await res.json();
+  console.log(data);
+  } catch (e){
+    console.log("ERROR!", e);
+  }
+};
+
+loadStarWarsPeople();
